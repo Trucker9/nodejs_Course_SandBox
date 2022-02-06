b76c352fc2a1b20833f08536f09dd774560a1b45
puzzlestudio1 <ali.fam0079@gmail.com>
2022-02-06 16:47:36 +0330

    getting ready for natours

---
+++ natours/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+/////////////////////////////////////////////
+
+// Response to get() http method app.get( <route> , <function(request, response)> );
+app.get("/", function (request, response) {
+  response.status(200).send("Hello from server");
+});
+
+
+
+
+
+
+
+
+// Start up the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port} ...`);
+});
+
