1c10bbcbab9eda0eea3d8317cecaf307050165d7
puzzlestudio1 <ali.fam0079@gmail.com>
2022-02-19 04:22:56 +0330

    Adding secuirity 🔐🔐

--- (3) natours/app.js
+++ (3) natours/app.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitizer = require('mongo-sanitizer');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const userRouter = require('./routes/userRoutes');
@@ -11,25 +15,62 @@ const globalErrorHandler = require('./controllers/errorController');
 const app = express();
 
 //############################################
-//############################################ Middlewares (apply on all requests)
+//############################################ Global Middlewares (apply on all requests)
 // Using middleware: req & res Objects go through middleware stack like a pipe line. here we are adding express.json() to the middleware stack.
 // each middleware will send these objects to next( by calling next(); ) middleware and the last one will send it to the client.
 // order of middleware is based on what order we write them.
 // Middleware applies to all request on the server and route functions are some kind of middleware which end the pipeline by sending back the result to the client.
 // NOTE: IF WE WANT TO APPLY A METHOD ON ALL REQUESTS, we define it as middleware, and write it before any routing middleware
 
-// Enabling morgan only in development
+// Security: Using default helmet settings. Each node app must have this.
+app.use(helmet());
+
+// 100 requests from same IP in 1 hour (3600 ms) - NOTE: resets on app restart.
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, pease try again in an hour!',
+});
+app.use('/api', limiter);
+
+// development logging
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection.
+// removes all "$" and "." from the body.
+app.use(mongoSanitizer());
+
+// Data sanitization against XSS
+// removes injected HTML and JS data (later the value of properties will be placed in HTML and imagine if value was HTML! )
+app.use(xss());
 // Serving static files:
 app.use(express.static(`${__dirname}/public/`)); // now we can access it in browser with only file name: localhost/overview.html
+
+// HTTP Parameters Pollution --- Sending http request with parameters that are not valid, may cause errors.
+// whitelist: allows the parameters in the array to be duplicated in the parameters.
+const hppOptions = {
+  whitelist: [
+    'duration',
+    'ratingsQuantity',
+    'ratingsAverage',
+    'maxGroupSize',
+    'difficulty',
+    'price',
+  ],
+};
+app.use(hpp());
+
 // Creating custom middleware:
 app.use((req, res, next) => {
   // console.log('Hello from the middleware 😀');
   // sending to the next middleware
   next();
 });
+
+// logging request time.
 app.use((req, res, next) => {
   // We used this when we sent the result.
   req.requestTime = new Date().toISOString();
